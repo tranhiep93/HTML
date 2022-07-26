19f8ca2fa805da6917dda6f4815a517c022135d0
tranhiep93 <tranhiep.091@gmail.com>
2022-07-26 20:45:25 +0700

    s

--- js.c02/js/string-with-quotes.js
+++ js.c02/js/string-with-quotes.js
@@ -7,7 +7,7 @@ title = "Molly's Special offers";
 message = '<a href=\"sale.html\">25% off!</a>';
 
 var elTitle = document.getElementById( 'title');
-eltitle.textContent = title;
+elTitle.textContent = title;
 
 var elNote = document.getElementById( 'note');
 elNote.innerHTML = message;
