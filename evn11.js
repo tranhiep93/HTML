0211a64bdaafbf794a6a275d89fc65d53732f50d
tranhiep93 <106593009+tranhiep93@users.noreply.github.com>
2022-07-28 13:04:57 +0700

    Update evn11.js

--- evn11.js
+++ evn11.js
@@ -149,26 +149,6 @@ function drawPrintEVN(trans) {
 
 const khoiTao = async () => {
     await sleep(1000);
-    const txtTien = $('<input type="text" id="txt_tien_value" name="txt_tien_value" required="" data-parsley-trigger="change" data-parsley-length="[1,100]">')
-    //const btnSave = $('<button class="btn btn-info" type="button" onclick="saveWallet()" id="btn-save" disabled = true><i class="fa fa-save"></i> Lưu HĐ</button>');
-    const btnShow = $('<button class="btn btn-primary" type="button" onclick="showWallet()" id="btn-eye"><i class="fa fa-eye"></i> Xem</button>');
-    const btnLock = $('<button class="btn btn-success" type="button" onclick="unLock()" id="btn-lock"><i class="fa fa-lock"></i> Xóa</button>');
-    const btnTraLai = $('<button class="btn btn-secondary" type="button" onclick="traLai()" id="btn-minus">Cộng</button>');
-    const btnSaoChep = $('<button class="btn btn-primary" type="button" onclick="doCopy()" id="btn-copy"><i class="fa fa-copy"></i> Copy</button>');
-    const lblTongCong = $('<span id="lbl_tien" style="font-size: 28px; font-weight: 900;">0</span>');
-
-
-    //$("#wallet-current-balance").parent().append(btnSave);
-    $("#wallet-current-balance").parent().append(btnShow);
-    $("#wallet-current-balance").parent().append(btnLock);
-    $("#wallet-current-balance").parent().append(txtTien);
-    $("#wallet-current-balance").parent().append(btnTraLai);
-    $("#wallet-current-balance").parent().append(lblTongCong);
-
-    $("#btn-reset").parent().before(btnSaoChep);
-    
-
-    await sleep(1000);
     
     $('#bill-template-evn-1').html(`
     <div class="bill-container" style="font-size: 13px;">
