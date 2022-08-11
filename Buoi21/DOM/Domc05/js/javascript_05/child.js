2c2218914d457940c2e54271f4f66706b3f8193c
tranhiep93 <tranhiep.091@gmail.com>
2022-08-12 05:49:17 +0700

    dom

---
+++ Buoi21/DOM/Domc05/js/javascript_05/child.js
@@ -0,0 +1,6 @@
+var startItem = document.getElementsByTagName('ul');
+var firstItem = startItem.firstChild;
+var lastItem = startItem.lastChild;
+
+firstItem.className = 'complete';
+lastItem.className = 'cool';
