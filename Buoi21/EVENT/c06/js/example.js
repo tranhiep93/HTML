3880263ab0e811fb11c481dfabeabb78ececa69c
tranhiep93 <tranhiep.091@gmail.com>
2022-08-12 15:08:17 +0700

    even

--- Buoi21/EVENT/c06/js/example.js
+++ Buoi21/EVENT/c06/js/example.js
@@ -10,3 +10,39 @@ function  writeLabel (e){
     textEntered = target.value;
     noteName.textContent = textEntered;
 }
+function recorderControls(e){
+    if (!e){
+        e = window.event;
+    }
+    target = e.target || e.srcElement;
+    if (e.preventDefault){
+        e.preventDefault();
+    } else {
+        e.returnValue = false;
+    }
+    switch (target.getAttribute('data-state')){
+        case 'record':
+            record(target);
+            break;
+        case 'stop':
+            stop(target);
+            break;
+    }
+}
+function  record(target){
+    target.setAttribute('data-state','stop');
+    target.textContent='stop';
+}
+function stop(target){
+    target.setAttribute('data-state','record');
+    target.textContent='record';
+}
+if (document.addEventListener){
+    document.addEventListener('click',function (e){
+        recorderControls(e);
+    },false);
+    noteInput.addEventListener('onclick',function (e){
+        recorderControls(e);
+    });
+    noteInput.attachEvent('conkeyup',writeLabel);
+}
